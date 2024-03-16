5f0a75db29a874be67f16567b88ff5c41e55dcf5
Ghazalkhan1 <ghazalkhan1211@gmail.com>
2024-03-16 15:04:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//Data Types
+//String
+let message = "Ghazal";
+console.log(message);
+//Number
+let myNumber = 5;
+console.log(myNumber);
+//Boolean
+let sunnyDay = true;
+console.log(sunnyDay);
+//Null
+let nullType = null;
+console.log(nullType);
+//undefined
+let undefinedType = undefined;
+console.log(undefinedType);
+//Any
+let anyMessage = "Let's go";
+console.log(anyMessage);
+//Bigint
+let bigNumber = 150000n;
+console.log(bigNumber);
+//Symbol
+let mySymbol = Symbol("hi");
+console.log(mySymbol);
+export {};
---
+++ app.ts
@@ -0,0 +1,38 @@
+//Data Types
+
+//String
+let message:string = "Ghazal";
+console.log(message);
+
+//Number
+let myNumber:number = 5;
+console.log(myNumber);
+
+//Boolean
+let sunnyDay:boolean = true;
+console.log(sunnyDay);
+
+//Null
+let nullType:null = null;
+console.log(nullType);
+
+//undefined
+let undefinedType:undefined = undefined;
+console.log(undefinedType);
+
+//Any
+let anyMessage:any = "Let's go";
+console.log(anyMessage);
+
+//Bigint
+let bigNumber:bigint = 150000n;
+console.log(bigNumber);
+
+//Symbol
+let mySymbol:symbol = Symbol("hi");
+console.log(mySymbol);
+
+
+
+
+
